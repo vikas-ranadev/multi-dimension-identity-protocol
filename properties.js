5b0c4a9638e64760566602eb3d7d566232f8d7d6
Vikas bhandary <vikas@ranadev.io>
2023-07-26 16:39:07 +0545

    updated properties fetch logic using environment variables (for using p2p_transport)

--- properties.js
+++ properties.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
 const propertiesReader = require('properties-reader');
 const path = require('path');
 const debug = require('debug')('mdip:properties');
-const dotenv = require('dotenv');
 
-const possibleEnvs = ['mdip', 'deps', 'btc', 'etc', 'omni', 'ipfs'];
+const possibleEnvs = [
+  'mdip',
+  'deps',
+  'btc',
+  'etc',
+  'omni',
+  'ipfs',
+  'p2p_transport',
+];
 let props;
 try {
   // following should work if environment have ./bin/etc/local.conf file
@@ -13,17 +21,25 @@ try {
   props = propertiesReader(path.join(`${__dirname}/local.conf`));
 }
 
-dotenv.config();
 process.env.BUILD_DATE = new Date().toISOString();
 debug(process.env.BUILD_DATE);
+const getModuleKeyName = (key) => {
+  const keyArr = key.split('_');
+  const moduleName = keyArr.length === 3 ? `${keyArr[0]}_${keyArr[1]}` : keyArr[0];
+  const name = keyArr.length === 3
+    ? `${keyArr[0]}_${keyArr[1]}.${keyArr[2]}`
+    : key.replace('_', '.');
+  return {
+    name,
+    moduleName,
+  };
+};
 
-Object.entries(process.env).map(([key, value]) => {
-  const moduleName = key.split('_')[0];
-  const name = key.replace('_', '.');
+Object.entries(process.env).forEach(([key, value]) => {
+  const { name, moduleName } = getModuleKeyName(key);
   if (possibleEnvs.includes(moduleName)) {
     props.set(name, value);
   }
-  return value;
 });
 
 debug(props.getAllProperties());
