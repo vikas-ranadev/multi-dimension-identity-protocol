a8454d1d9ab0b979309c4bcb9b519e7bc1420253
David McFadzean <davidmc@gmail.com>
2023-12-01 14:06:08 -0500

    Adds unit test for /serverinfo

--- test/api.test.js
+++ test/api.test.js
@@ -3,7 +3,13 @@ const express = require('express');
 const daemonRouter = require('../lib/node/api.routes');
 
 const app = express();
-app.use('/', daemonRouter); // assuming your daemonRouter is at '/api' path
+app.use('/', daemonRouter);
+
+const util = require('../lib/utils/util');
+
+jest.mock('../lib/utils/util', () => ({
+  checkConnections: jest.fn(),
+}));
 
 describe('GET /getuptime', () => {
   it('responds with uptime', async () => {
@@ -13,5 +19,35 @@ describe('GET /getuptime', () => {
     expect(response.body).toHaveProperty('uptime');
     expect(Number.isInteger(response.body.uptime)).toBeTruthy();
     expect(response.body.uptime).toBeGreaterThan(0);
+  });
+});
+
+describe('GET /serverinfo', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('responds with serverinfo', async () => {
+    const mockServerInfo = { info: 'mock info' };
+    util.checkConnections.mockResolvedValue(mockServerInfo);
+
+    const response = await request(app).get('/serverinfo');
+
+    expect(response.statusCode).toBe(200);
+    expect(util.checkConnections).toHaveBeenCalled();
+    expect(response.body.error).toBeNull();
+    expect(response.body.result).toEqual(mockServerInfo);
+  });
+
+  it('responds with an error when checkConnections fails', async () => {
+    const mockError = new Error('mock error');
+    util.checkConnections.mockRejectedValue(mockError);
+
+    const response = await request(app).get('/serverinfo');
+
+    expect(response.statusCode).toBe(500);
+    expect(util.checkConnections).toHaveBeenCalled();
+    expect(response.body.error).toEqual(mockError.message);
+    expect(response.body.result).toBeNull();
   });
 });
